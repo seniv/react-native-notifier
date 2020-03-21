0d24f1b95c2473117a5c92b58318874bd9704984
Ivan Seniv <seniv97@gmail.com>
2020-03-21 23:45:36 +0200

    Fix iOS and Android example build

--- example/android/app/src/main/java/com/NotifierExample/MainApplication.java
+++ example/android/app/src/main/java/com/NotifierExample/MainApplication.java
@@ -10,8 +10,6 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-import com.reactnativenotifier.NotifierPackage;
-
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -25,9 +23,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for NotifierExample:
-          // packages.add(new MyReactNativePackage());
-          packages.add(new NotifierPackage());
 
           return packages;
         }
