4bff0bb4670402447deb7d0575f594c664f58674
Ivan Seniv <seniv97@gmail.com>
2024-07-21 00:17:47 +0300

    feat: allow to pass ref into NotifierWrapper component

--- src/NotifierWrapper.tsx
+++ src/NotifierWrapper.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { NotifierRoot } from './Notifier';
 import type { ShowNotificationParams } from './types';
 
@@ -6,12 +6,11 @@ interface NotifierWrapperProps extends ShowNotificationParams {
   children: React.ReactNode;
 }
 
-export const NotifierWrapper = ({
-  children,
-  ...defaultParams
-}: NotifierWrapperProps) => (
-  <>
-    {children}
-    <NotifierRoot {...defaultParams} />
-  </>
+export const NotifierWrapper = forwardRef<NotifierRoot, NotifierWrapperProps>(
+  ({ children, ...defaultParams }, ref) => (
+    <>
+      {children}
+      <NotifierRoot ref={ref} {...defaultParams} />
+    </>
+  )
 );
